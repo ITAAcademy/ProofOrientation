db47d49a7d93a4669f132cd5c6ddf99b3858e8ed
newKeyla <mamoluysya@gmail.com>
2015-04-01 01:46:11 +0300

    create skeleton of ProfOrientation project

---
+++ js/router.js
@@ -0,0 +1,80 @@
+function route(path,data){
+ switch (path){
+  case 'greetings':
+  {
+   getData(
+    function (data)
+    {
+     $("#results").html(data);
+    },
+    {"data":data}
+    
+   );
+  }
+  break;
+  case 'tests':
+  {
+   getTests(
+    function (data)
+    {
+     $("#start_test").html(data);
+    },
+    {"data":data}
+
+   );
+  }
+  break;
+  /*
+  case 'conclusion':
+  {
+   getData(
+    function (html)
+    {
+     $("#results").html(html);
+    },
+    {"data":data},
+    "http://localhost/prooforientation/web/index3.php"
+   );
+  }
+  break;*/
+}
+}
+
+function getData(successFunction,request)
+{
+   url = location.href+"index.php/tests/tests";
+   $.ajax({
+   url: url,
+   cache: false,
+   crossDomain: true,
+   type: "POST",
+   data:request,
+   success: function(data){
+    successFunction(data);
+     },
+   error: function (errormessage) {
+               alert("Error");
+               }
+   });
+}
+
+function getTests(successFunction,request)
+{
+	
+   url = location.href+"index.php/tests/testsJSON";
+   $.ajax({
+   url: url,
+   cache: false,
+   crossDomain: true,
+   type: "POST",
+   data:request,
+   dataType: 'json',
+   success: function(data){
+	   console.log(data);
+     successFunction(data);
+     },
+   error: function (errormessage) {
+               alert("Error");
+               }
+   });
+}
