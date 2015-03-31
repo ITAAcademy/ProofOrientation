1b51eb03c8f60b9a23168c85451c96219c709766
AndreyS <andrushtadler@gmail.com>
2015-03-31 18:49:13 +0300

    -clear master

--- web/js/main-index.js
+++
@@ -1,3 +0,0 @@
-/*
-this is index js file
- */
--- web/js/main.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-// get the click of the create button -Shtadler-
-    $('#modalButton').click(function(){
-        $('#modal').modal('show')
-            .find('#modalContent')
-            .load($(this).attr('value'));
-    });
-});
