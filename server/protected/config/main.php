18a2f2b9ccc7a44a81d1e834edf68afe84334097
root <root@itatests.com>
2015-04-01 06:37:16 +0000

    Fix urlMAnager

--- server/protected/config/main.php
+++ server/protected/config/main.php
@@ -40,14 +40,14 @@ return array(
 
 		// uncomment the following to enable URLs in path-format
 		
-		'urlManager'=>array(
-			'urlFormat'=>'path',
-			'rules'=>array(
-				'<controller:\w+>/<id:\d+>'=>'<controller>/view',
-				'<controller:\w+>/<action:\w+>/<id:\d+>'=>'<controller>/<action>',
-				'<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
-			),
-		),
+		//'urlManager'=>array(
+		//	'urlFormat'=>'path',
+		//	'rules'=>array(
+		//		'<controller:\w+>/<id:\d+>'=>'<controller>/view',
+		//		'<controller:\w+>/<action:\w+>/<id:\d+>'=>'<controller>/<action>',
+		//		'<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
+		//	),
+		//),
 		
 
 		// database settings are configured in database.php
